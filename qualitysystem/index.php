91e7ab55f59e0613afd6f6774d8980b5adbba814
cedrickjames <cedrickorozo@gmail.com>
2023-10-24 19:30:01 +0800

    update

--- qualitysystem/index.php
+++ qualitysystem/index.php
@@ -547,7 +547,7 @@ activepage.classList.add("text-cyan-500");
                         <path fill-rule="evenodd"
                             d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                             clip-rule="evenodd"></path>
-                    </svg></span>Continuously improving our quality system to meet our customers’. expectations and meet the ISO9000 and IATF standards.
+                    </svg></span>Continuously improving our quality system to meet our customers’ expectations and meet the ISO9000 and IATF standards.
             </p> <p class="flex mb-4 text-white text-sm  md:text-5xl lg:text-3xl"> <span  style="color: #ffffff61;">  <svg class="w-10 h-10 text-green-500 dark:text-green-400 " fill="currentColor"
                         viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                         <path fill-rule="evenodd"
